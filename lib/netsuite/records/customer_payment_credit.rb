644ca059bb23023cac5c5532d36b31b74b9651c4
Andrei Istratii <andrei.istratii@shopify.com>
2018-08-28 10:04:12 -0400

    Add CustomerPaymentCreditList to CustomerPayment

--- lib/netsuite/records/customer_payment_credit.rb
+++ lib/netsuite/records/customer_payment_credit.rb
@@ -1,12 +1,12 @@
 module NetSuite
   module Records
-    class CustomerPaymentApply
+    class CustomerPaymentCredit
       include Support::Fields
       include Support::Records
       include Namespaces::TranCust
 
-      fields :amount, :apply, :apply_date, :currency, :disc, :disc_amt, :disc_date,
-             :doc, :due, :job, :line, :ref_num, :total, :type
+      fields :amount, :applied_to, :apply, :credit_date, :currency,
+             :doc, :due, :line, :ref_num, :total, :type
 
       def initialize(attributes = {})
         initialize_from_attributes_hash(attributes)
